047fa59911241fcbbe560c9065f4af461cb11fd7
Shashank <f20200073@pilani.bits-pilani.ac.in>
2023-02-02 01:33:21 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import './App.css';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 function App() {
+
   const [inputText, setInputText] = useState('');
-  const [urlList, setUrlList] = useState([]);
-  const handleInputChnage = (event) => {
-    // console.log(event.target.value)
-    const lines = event.target.value.split(/[.\n]+/)
-    console.log(lines);
-  }
+  const [userPrompt, setUserPrompt] = useState("");
+  const [result, setResult] = useState("");
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: userPrompt,
+      n: 3,
+      size: "512x512",
+    });
+    console.log(res.data.data[0].url);
+    console.log(res.data.data);
+    setResult(res.data.data);
+  };
   return (
     <div className="App">
       <div className='main'>
-        <textarea rows="30" cols="80" name="description" placeholder='Enter input here...' onChange={handleInputChnage}>
-          {inputText}
-        </textarea>
-
-        <div className='ImageList'>
-          
-        </div>
-
+        <textarea rows="30" cols="80" name="description" value={inputText} placeholder='Enter input here...' onChange={(event) => {
+          const lines = event.target.value.split(/[.\n]+/)
+          console.log(lines);
+          setUserPrompt(event.target.value)
+          setInputText(event.target.value)
+        }}/>
+        {/* <div className='ImageList'>
+          {
+            result.forEach((image, index) => {
+              <img className="result-image" src={image.url} alt="result" key={index} />
+            })
+          }
+        </div> */}
+        <button onClick={generateImage}></button>
+        {result.length > 0 ? (
+            result.map((image, index) => (
+              <img key={index} src={image.url} alt="Image" />
+            ))
+        ) : (
+          <></>
+        )}
       </div>
     </div>
-  );
+  )
 }
-
 export default App;
