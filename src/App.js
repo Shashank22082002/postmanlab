58f0f5a5283624fd5f0e2e10b9aa7dac4555752d
Shashank <f20200073@pilani.bits-pilani.ac.in>
2023-02-01 23:39:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [urlList, setUrlList] = useState([]);
+  const handleInputChnage = (event) => {
+    // console.log(event.target.value)
+    const lines = event.target.value.split(/[.\n]+/)
+    console.log(lines);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <textarea rows="30" cols="80" name="description" placeholder='Enter input here...' onChange={handleInputChnage}>
+          {inputText}
+        </textarea>
+
+        <div className='ImageList'>
+          
+        </div>
+
+      </div>
     </div>
   );
 }
